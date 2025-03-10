4b93a830afeaa9b7c3f39595eeb59320dd9f37f9
Odii Daniel <xenithheight@gmail.com>
2025-03-10 20:01:38 +0100

    update again

--- layouts/fixed.vue
+++ layouts/fixed.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class=" fixed bg-purple-500 top-0 bottom-10 left-0 w-full h-full border !border-yellow-500">
+    <div class=" fixed bg-purple-500 top-0 bottom-10 left-0 w-full h-full border !border-yellow-500 flex flex-col">
+        <TheNavbar/>
         <slot/>
     </div>
 </template>
