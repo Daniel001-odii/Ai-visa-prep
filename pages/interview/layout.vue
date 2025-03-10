460141a02a9859476c9f48dacec7d44e3fbf4abe
Odii Daniel <xenithheight@gmail.com>
2025-03-10 14:07:20 +0100

    test layout

--- pages/interview/layout.vue
+++ pages/interview/layout.vue
@@ -1,7 +1,7 @@
 <template>
     <div class=" min-h-screen flex flex-col justify-between">
         <div class=" bg-red-500 w-full p-5">navbar</div>
-        <div class=" bg-green-500 w-full h-[200px] p-12">
+        <div class=" bg-green-500 w-full p-12">
             <div class=" bg-blue-500 p-5">chat box</div>
         </div>
     </div>
